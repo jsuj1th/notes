da3ad39659d6520ad3abaa0afcb2c2129a020b1d
suj1thj <s02@tamu.edu>
2025-07-15 13:27:46 +0530

    Added login and logout functionality

--- home/urls.py
+++ home/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.HomeView.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='home'),
     # path('authorized/', views.authorized, name='authorized'),
     path('authorized/', views.AuthorizedView.as_view(), name='authorized'),
     path('advantages/', views.AdvantagesView.as_view(), name='advantages'),
+    path('login/', views.LoginInterface.as_view(), name='login'),
+    path('logout/', views.LogoutInterface.as_view(), name='logout'),
 ]
